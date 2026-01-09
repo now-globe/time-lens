e95cde6296690ad7302644b9823276dce7d6b52c
phuoctaiismee <taichet2003@gmail.com>
2026-01-09 08:08:34 +0700

    feat: improment layout mobile

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import NormalClock from "@/components/common/clocks/normal";
 
 export default function Home() {
     return (
-        <div className="flex min-h-screen items-center justify-center">
+        <div className="flex h-svh items-center justify-center">
             <NormalClock showSeconds format12h={false} className="text-5xl lg:text-8xl font-semibold tracking-widest transition-all duration-100 ease-in-out" />
         </div>
     );
